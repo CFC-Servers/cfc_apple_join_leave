a988640253c47688c2d2e31070da9aeec37c8282
Bleck <OverlordBleck@gmail.com>
2019-08-10 20:34:28 -0500

    Refactoring PlayerInitialSpawn

--- lua/autorun/sv/sv_init.lua
+++ lua/autorun/sv/sv_init.lua
@@ -1,4 +1,5 @@
 util.AddNetworkString( "cfc_playerconnect" )
+util.AddNetworkString( "cfc_playerinitialspawn" )
 
 local function onPlayerConnect( plyName, ip )
     MsgN( "Player ".. plyName .. " has connected to the server." )
@@ -9,3 +10,13 @@ local function onPlayerConnect( plyName, ip )
 end
 
 hook.Add( "PlayerConnect", "CFC_OnPlayerConnect", onPlayerConnect )
+
+local function onPlayerInitialSpawn( ply )
+    if not IsValid( ply ) then return end
+
+    net.Start( "cfc_playerinitialspawn" )
+        net.WriteEntity( ply )
+    net.Broadcast()
+end
+
+hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn", onPlayerInitialSpawn )
