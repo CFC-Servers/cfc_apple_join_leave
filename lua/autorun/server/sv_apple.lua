ff30242b0fb6875bd12ba7d8e42d4b659e6498d6
Bleck <OverlordBleck@gmail.com>
2019-08-10 22:43:34 -0500

    Hotfixes, fixing player disconnecting

--- lua/autorun/server/sv_apple.lua
+++ lua/autorun/server/sv_apple.lua
@@ -4,16 +4,6 @@ util.AddNetworkString( "cfc_playerinitialspawn" )
 util.AddNetworkString( "cfc_playerdisconnect" )
 local playerList = playerList or {}
 
-local function fetchDisconnectedPlayer( steamID )
-    for _, ply in pairs( playerList ) do
-        if ply:SteamID() == steamID then
-            table.RemoveByValue( playerList, ply )
-
-            return ply
-        end
-    end
-end
-
 local function onPlayerConnect( plyName, ip )
     MsgN( "Player ".. plyName .. " has connected to the server." )
 
@@ -29,9 +19,11 @@ local function onPlayerInitialSpawn( ply )
     MsgN( ply:Name() .. " has spawned in the server." )
     table.insert( playerList, ply )
 
-    net.Start( "cfc_playerinitialspawn" )
-        net.WriteEntity( ply )
-    net.Broadcast()
+    timer.Simple( 3, function()
+        net.Start( "cfc_playerinitialspawn" )
+            net.WriteEntity( ply )
+        net.Broadcast()
+    end)
 end
 
 hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn", onPlayerInitialSpawn )
@@ -39,16 +31,15 @@ hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn", onPlayerInitialSpawn )
 local function onPlayerDisconnect( data )
     local name = data.name
     local steamID = data.networkid
-    local ply = fetchDisconnectedPlayer( steamID )
     local isBot = data.bot
     local reason = data.reason
     if isBot then return end
 
     MsgN( "Player " .. name .. " has left the server. (" .. reason .. ")" )
 
-    if ply == nil then return end
     net.Start( "cfc_playerdisconnect" )
-        net.WriteEntity( ply )
+        net.WriteString( name )
+        net.WriteString( steamID )
         net.WriteString( reason )
     net.Broadcast()
 end
