5ec077a65d751cf68f535a7023e1d444685631f6
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-31 16:16:22 +0100

    Move disconnect to client

--- lua/autorun/server/sv_apple.lua
+++ lua/autorun/server/sv_apple.lua
@@ -1,10 +1,9 @@
-gameevent.Listen( "player_disconnect" )
 util.AddNetworkString( "cfc_playerconnect" )
 util.AddNetworkString( "cfc_playerinitialspawn" )
 util.AddNetworkString( "cfc_playerdisconnect" )
 
-local function onPlayerConnect( plyName, ip )
-    MsgN( "Player ".. plyName .. " has connected to the server." )
+local function onPlayerConnect( plyName )
+    MsgN( "Player " .. plyName .. " has connected to the server." )
 
     net.Start( "cfc_playerconnect" )
         net.WriteString( plyName )
@@ -32,24 +31,3 @@ local function onPlayerInitialSpawn( ply )
 end
 
 hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn", onPlayerInitialSpawn )
-
-local function onPlayerDisconnect( data )
-    local name = data.name
-    local steamID = data.networkid
-    local userID = data.userid
-    local isBot = data.bot
-    local reason = data.reason
-    local ply = Player( userID )
-    local plyTeam = IsValid( ply ) and ply:Team() or TEAM_UNASSIGNED
-    
-    MsgN( "Player " .. name .. " has left the server. (" .. reason .. ")" )
-
-    net.Start( "cfc_playerdisconnect" )
-        net.WriteString( name )
-        net.WriteString( steamID )
-        net.WriteString( reason )
-        net.WriteInt( plyTeam, 11 )
-    net.Broadcast()
-end
-
-hook.Add( "player_disconnect", "CFC_OnPlayerDisconnect", onPlayerDisconnect )
