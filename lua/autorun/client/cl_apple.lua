7ca41bb550f2928593df4bae4d7c3d34e37eada8
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-31 17:29:19 +0100

    Move back to server and use uniqure net/hook names

--- lua/autorun/client/cl_apple.lua
+++ lua/autorun/client/cl_apple.lua
@@ -1,5 +1,3 @@
-gameevent.Listen( "player_disconnect" )
-
 local function formatTime( seconds )
     seconds = 312
     if seconds < 61 then
@@ -12,13 +10,13 @@ local function formatTime( seconds )
     return string.format( "took %dm %02ds", minutes, seconds )
 end
 
-net.Receive( "cfc_playerconnect", function()
+net.Receive( "cfc_playerconnect_ajl", function()
     local name = net.ReadString()
 
     chat.AddText( Color( 255, 0, 255 ), "[Server] ", Color( 255, 255, 255 ), name .. " has connected to the server." )
 end)
 
-net.Receive( "cfc_playerinitialspawn", function()
+net.Receive( "cfc_playerinitialspawn_ajl", function()
     local name = net.ReadString()
     local sID = net.ReadString()
     local plyTeam = net.ReadInt( 11 )
@@ -28,16 +26,12 @@ net.Receive( "cfc_playerinitialspawn", function()
     chat.AddText( Color( 255, 0, 255 ), "[Server] ", teamColor, name, Color( 255, 255, 255 ), " (" .. sID .. ") has spawned in the server (" .. formatTime( joinTime ) .. ")." )
 end)
 
-local function onPlayerDisconnect( data )
-    local name = data.name
-    local steamID = data.networkid
-    local userID = data.userid
-    local reason = data.reason
-    local ply = Player( userID )
-    local plyTeam = IsValid( ply ) and ply:Team() or TEAM_UNASSIGNED
+net.Receive( "cfc_playerdisconnect_ajl", function( len )
+    local name = net.ReadString()
+    local sID = net.ReadString()
+    local reason = net.ReadString()
+    local plyTeam = net.ReadInt( 11 )
     local teamCol = team.GetColor( plyTeam )
 
-    chat.AddText( Color( 255, 0, 255 ), "[Server] ", teamCol, name, Color( 255, 255, 255 ), " (" .. steamID .. ") has left the server. (" .. reason .. ")" )
-end
-
-hook.Add( "player_disconnect", "CFC_OnPlayerDisconnect", onPlayerDisconnect )
+    chat.AddText( Color( 255, 0, 255 ), "[Server] ", teamCol, name, Color( 255, 255, 255 ), " (" .. sID .. ") has left the server. (" .. reason .. ")" )
+end)
--- lua/autorun/server/sv_apple.lua
+++ lua/autorun/server/sv_apple.lua
@@ -1,7 +1,8 @@
 gameevent.Listen( "player_connect" )
-util.AddNetworkString( "cfc_playerconnect" )
-util.AddNetworkString( "cfc_playerinitialspawn" )
-util.AddNetworkString( "cfc_playerdisconnect" )
+gameevent.Listen( "player_disconnect" )
+util.AddNetworkString( "cfc_playerconnect_ajl" )
+util.AddNetworkString( "cfc_playerinitialspawn_ajl" )
+util.AddNetworkString( "cfc_playerdisconnect_ajl" )
 
 local function onPlayerConnect( data )
     timer.Simple( 0, function() -- Gotta wait a single tick otherwise the player doesn't exist yet
@@ -11,13 +12,13 @@ local function onPlayerConnect( data )
 
         MsgN( "Player " .. plyName .. " has connected to the server." )
 
-        net.Start( "cfc_playerconnect" )
+        net.Start( "cfc_playerconnect_ajl" )
             net.WriteString( plyName )
         net.Broadcast()
     end)
 end
 
-hook.Add( "player_connect", "CFC_OnPlayerConnect", onPlayerConnect )
+hook.Add( "player_connect", "CFC_OnPlayerConnect_AppleJoinLeave", onPlayerConnect )
 
 local function onPlayerInitialSpawn( ply )
     if not IsValid( ply ) then return end
@@ -30,7 +31,7 @@ local function onPlayerInitialSpawn( ply )
         local plyTeam = ply:Team()
         local joinTime = CurTime() - ply.AppleConnect
 
-        net.Start( "cfc_playerinitialspawn" )
+        net.Start( "cfc_playerinitialspawn_ajl" )
             net.WriteString( name )
             net.WriteString( steamID )
             net.WriteInt( plyTeam, 11 )
@@ -39,4 +40,24 @@ local function onPlayerInitialSpawn( ply )
     end)
 end
 
-hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn", onPlayerInitialSpawn )
+hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn_AppleJoinLeave", onPlayerInitialSpawn )
+
+local function onPlayerDisconnect( data )
+    local name = data.name
+    local steamID = data.networkid
+    local userID = data.userid
+    local reason = data.reason
+    local ply = Player( userID )
+    local plyTeam = IsValid( ply ) and ply:Team() or TEAM_UNASSIGNED
+
+    MsgN( "Player " .. name .. " has left the server. (" .. reason .. ")" )
+
+    net.Start( "cfc_playerdisconnect_ajl" )
+        net.WriteString( name )
+        net.WriteString( steamID )
+        net.WriteString( reason )
+        net.WriteInt( plyTeam, 11 )
+    net.Broadcast()
+end
+
+hook.Add( "player_disconnect", "CFC_OnPlayerDisconnect_AppleJoinLeave", onPlayerDisconnect ) 
