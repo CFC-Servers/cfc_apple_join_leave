576cc56b62eea7bd761e44b42ffa9ee3de53f5d9
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-31 16:16:22 +0100

    Move disconnect to client

--- lua/autorun/client/cl_apple.lua
+++ lua/autorun/client/cl_apple.lua
@@ -1,10 +1,12 @@
+gameevent.Listen( "player_disconnect" )
+
 net.Receive( "cfc_playerconnect", function( len )
     local name = net.ReadString()
 
     chat.AddText( Color( 255, 0, 255 ), "[Server] ", Color( 255, 255, 255 ), name .. " has connected to the server." )
 end)
 
-net.Receive( "cfc_playerinitialspawn", function( len )
+net.Receive( "cfc_playerinitialspawn", function()
     local name = net.ReadString()
     local sID = net.ReadString()
     local plyTeam = net.ReadInt( 11 )
@@ -13,12 +15,16 @@ net.Receive( "cfc_playerinitialspawn", function( len )
     chat.AddText( Color( 255, 0, 255 ), "[Server] ", teamColor, name, Color( 255, 255, 255 ), " (" .. sID .. ") has spawned in the server." )
 end)
 
-net.Receive( "cfc_playerdisconnect", function( len )
-    local name = net.ReadString()
-    local sID = net.ReadString()
-    local reason = net.ReadString()
-    local plyTeam = net.ReadInt( 11 )
+local function onPlayerDisconnect()
+    local name = data.name
+    local steamID = data.networkid
+    local userID = data.userid
+    local reason = data.reason
+    local ply = Player( userID )
+    local plyTeam = IsValid( ply ) and ply:Team() or TEAM_UNASSIGNED
     local teamCol = team.GetColor( plyTeam )
 
-    chat.AddText( Color( 255, 0, 255 ), "[Server] ", teamCol, name, Color( 255, 255, 255 ), " (" .. sID .. ") has left the server. (" .. reason .. ")" )
-end)
+    chat.AddText( Color( 255, 0, 255 ), "[Server] ", teamCol, name, Color( 255, 255, 255 ), " (" .. steamID .. ") has left the server. (" .. reason .. ")" )
+end
+
+hook.Add( "player_disconnect", "CFC_OnPlayerDisconnect", onPlayerDisconnect )
