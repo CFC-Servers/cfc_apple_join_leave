f30058bfd9e43880b3f8cf4f6794710411930d3f
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-01-31 17:11:25 +0100

    Add spawn in times to initial spawn messages

--- lua/autorun/server/sv_apple.lua
+++ lua/autorun/server/sv_apple.lua
@@ -1,16 +1,23 @@
+gameevent.Listen( "player_connect" )
 util.AddNetworkString( "cfc_playerconnect" )
 util.AddNetworkString( "cfc_playerinitialspawn" )
 util.AddNetworkString( "cfc_playerdisconnect" )
 
-local function onPlayerConnect( plyName )
-    MsgN( "Player " .. plyName .. " has connected to the server." )
+local function onPlayerConnect( data )
+    timer.Simple( 0, function() -- Gotta wait a single tick otherwise the player doesn't exist yet
+        local plyName = data.name
+        local ply = Player( data.userid )
+        ply.AppleConnect = CurTime()
 
-    net.Start( "cfc_playerconnect" )
-        net.WriteString( plyName )
-    net.Broadcast()
+        MsgN( "Player " .. plyName .. " has connected to the server." )
+
+        net.Start( "cfc_playerconnect" )
+            net.WriteString( plyName )
+        net.Broadcast()
+    end)
 end
 
-hook.Add( "PlayerConnect", "CFC_OnPlayerConnect", onPlayerConnect )
+hook.Add( "player_connect", "CFC_OnPlayerConnect", onPlayerConnect )
 
 local function onPlayerInitialSpawn( ply )
     if not IsValid( ply ) then return end
@@ -21,11 +28,13 @@ local function onPlayerInitialSpawn( ply )
 
     timer.Simple( 3, function()
         local plyTeam = ply:Team()
+        local joinTime = CurTime() - ply.AppleConnect
 
         net.Start( "cfc_playerinitialspawn" )
             net.WriteString( name )
             net.WriteString( steamID )
             net.WriteInt( plyTeam, 11 )
+            net.WriteInt( joinTime, 13 )
         net.Broadcast()
     end)
 end
