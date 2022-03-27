e299fbdd4cb2ed973ab4afab279fe9c4482ad28f
wrefgtzweve <69946827+wrefgtzweve@users.noreply.github.com>
2022-03-27 20:02:26 +0200

    Fix join times and account for map change joins

--- lua/autorun/server/sv_apple.lua
+++ lua/autorun/server/sv_apple.lua
@@ -4,39 +4,44 @@ util.AddNetworkString( "cfc_playerconnect_ajl" )
 util.AddNetworkString( "cfc_playerinitialspawn_ajl" )
 util.AddNetworkString( "cfc_playerdisconnect_ajl" )
 
-local function onPlayerAuthed( ply )
-    local name = ply:GetName()
-    ply.AppleJLAuthTime = CurTime()
+local connectingSteamId = {}
+local serverStarTime = CurTime() -- Likely near 1 always, but worth to set anyway
 
-    MsgN( "Player " .. name .. " has connected to the server." )
+local function onPlayerConnect( data )
+    local plyName = data.name
+    connectingSteamId[data.networkid] = CurTime()
+
+    MsgN( "Player " .. plyName .. " has connected to the server." )
 
     net.Start( "cfc_playerconnect_ajl" )
-        net.WriteString( name )
+        net.WriteString( plyName )
     net.Broadcast()
 end
 
-hook.Add( "PlayerAuthed", "CFC_PlayerAuthed_AppleJoinLeave", onPlayerAuthed )
+hook.Add( "player_connect", "CFC_OnPlayerConnect_AppleJoinLeave", onPlayerConnect )
 
 local function onPlayerInitialSpawn( ply )
     if not IsValid( ply ) then return end
     local name = ply:Name()
     local steamID = ply:SteamID()
+    local plyTeam = ply:Team()
 
     MsgN( name .. " has spawned in the server." )
 
-    timer.Simple( 3, function()
-        if not IsValid( ply ) then return end
-        local plyTeam = ply:Team()
-        local joinTime = CurTime() - ( ply.AppleJLAuthTime or CurTime() ) + 1
+    -- player_connect only runs when people are connecting to the server. Not when joining from map switch, this checks for map switches.
+    local joinTime
+    if connectingSteamId[steamID] then
+        joinTime = CurTime() - connectingSteamId[steamID]
+    else
+        joinTime = CurTime() - serverStarTime
+    end
 
-        net.Start( "cfc_playerinitialspawn_ajl" )
-            net.WriteString( name )
-            net.WriteString( steamID )
-            net.WriteInt( plyTeam, 11 )
-            net.WriteInt( joinTime, 13 )
-        net.Broadcast()
-        ply.AppleJLAuthTime = nil
-    end)
+    net.Start( "cfc_playerinitialspawn_ajl" )
+        net.WriteString( name )
+        net.WriteString( steamID )
+        net.WriteInt( plyTeam, 11 )
+        net.WriteInt( joinTime, 13 )
+    net.Broadcast()
 end
 
 hook.Add( "PlayerInitialSpawn", "CFC_PlayerInitialSpawn_AppleJoinLeave", onPlayerInitialSpawn )
